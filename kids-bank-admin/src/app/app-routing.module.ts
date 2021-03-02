c79e5a21a78fabd3983b16363a91c01dfe00540d
mikezielony <mike.zielony@gmail.com>
2021-03-02 19:07:16 +0100

    Add

--- kids-bank-admin/src/app/app-routing.module.ts
+++ kids-bank-admin/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListAccountsComponent} from "./components/list-accounts/list-accounts.component";
 import { AddAccountComponent} from "./components/add-account/add-account.component";
+import {DetailsAccountComponent} from "./components/details-account/details-account.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'accounts', pathMatch: 'full' },
   { path: 'accounts', component: ListAccountsComponent },
-  { path: 'add', component: AddAccountComponent }
+  { path: 'add', component: AddAccountComponent },
+  { path: 'details', component: DetailsAccountComponent }
 ];
 
 @NgModule({
--- kids-bank-admin/src/app/components/details-account/details-account.component.ts
+++ kids-bank-admin/src/app/components/details-account/details-account.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { AccountService} from "../services/account.service";
+import { Account} from "../../models/account.model";
 
 @Component({
   selector: 'app-details-account',
   templateUrl: './details-account.component.html',
   styleUrls: ['./details-account.component.scss']
 })
-export class DetailsAccountComponent implements OnInit {
 
-  constructor() { }
+export class DetailsAccountComponent implements OnInit, OnChanges {
+@Input() account: Account;
+@Output() refreshList: EventEmitter<any> = new EventEmitter();
+currentAccount: Account = null;
+message = '';
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.message = '';
   }
 
+  ngOnChanges(): void {
+    this.message = '';
+    this.currentAccount = { ...this.account };
+  }
+
+  /*updatePublished(status): void {
+    this.accountService.update(this.currentAccount.name, { published: status })
+      .then(() => {
+        this.currentAccount.balance = status;
+        this.message = 'The status was updated successfully!';
+      })
+      .catch(err => console.log(err));
+  }*/
+
+  updateAccount(): void {
+    const data = {
+      name: this.currentAccount.name,
+      balance: this.currentAccount.balance
+    };
+
+    this.accountService.update(this.currentAccount.name, data)
+      .then(() => this.message = 'The account was updated successfully!')
+      .catch(err => console.log(err));
+  }
+
+  deleteAccount(): void {
+    this.accountService.delete(this.currentAccount.name)
+      .then(() => {
+        this.refreshList.emit();
+        this.message = 'The account was updated successfully!';
+      })
+      .catch(err => console.log(err));
+  }
 }
+
+
