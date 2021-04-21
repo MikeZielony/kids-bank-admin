c37cbcfc42aad8a8c84fcf123950a08e0cde9883
mikezielony <mike.zielony@gmail.com>
2021-04-21 06:24:53 +0200

    Refactor code

--- kids-bank-admin/src/app/components/login/login.component.ts
+++ kids-bank-admin/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/auth";
 import {Router} from '@angular/router';
 import firebase from 'firebase/app';
@@ -9,20 +9,27 @@ import {AuthMailModel} from "../../models/authMail.model";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
   authMail: AuthMailModel;
   authMailResults: AuthMailModel[] = [];
   isAuthenticated = false;
   isVisible = true;
+  route: string;
 
 
   constructor(public afAuth: AngularFireAuth, public router: Router) {
   }
 
+  ngOnInit(): void {
+        this.route = this.router.url;
+    }
+
   signIn(): void {
     const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-    this.afAuth.signInWithPopup(googleAuthProvider).then(() => this.router.navigate(['accounts']));
+    this.afAuth.signInWithPopup(googleAuthProvider).then(() => {
+      this.router.navigate(['accounts'])
+    });
   }
 
   signOut(): void {
